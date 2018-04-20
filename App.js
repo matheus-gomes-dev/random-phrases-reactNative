591b893f2a8205d67c444189983a0ef062438ea1
matheus-lima92 <lima.eca@gmail.com>
2018-04-20 00:08:56 -0300

    initial style implementation

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
 
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default class App extends React.Component {
+
+  //const { container } = Styles
+
   render() {
     return (
-      <Text style={styles.textStyle}>Day's phrase</Text>
+      <View style={Styles.container}>
+        <Image source={require('./img/logo.png')}/>
+        <TouchableOpacity>
+          <Text>Nova frase</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textStyle: {
-    fontSize: 20,
-    backgroundColor: '#08509B',
-    margin: 30,
-    textAlign: 'center'
-  }
-});
+
